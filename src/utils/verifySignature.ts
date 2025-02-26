23f839254abfc5b95234be7b6079b2a55e07461d
Ömer Toraman <toramanomer@icloud.com>
2025-02-26 21:34:11 +0300

    Enhance verifySignature function to validate key types and sizes for HMAC, RSA, ECDSA, and RSASSA-PSS algorithms

--- src/utils/verifySignature.ts
+++ src/utils/verifySignature.ts
@@ -2,26 +2,46 @@ import {
 	constants,
 	createHmac,
 	createVerify,
+	KeyObject,
 	timingSafeEqual
 } from 'node:crypto'
-import { AlgorithmParameterValue } from '@/utils/alg'
+
+import {
+	AlgorithmParameterValue,
+	ecdsaAlgParams,
+	hmacAlgParams,
+	rsaAlgParams,
+	rsaPssAlgParams
+} from '@/utils/alg'
 
 export const verifySignature = (
 	signingInput: string,
 	signature: Buffer,
 	algorithm: AlgorithmParameterValue,
-	key: Buffer
+	key: KeyObject
 ): boolean => {
 	switch (algorithm) {
 		// HMAC with SHA-2 Functions
 		case 'HS256':
 		case 'HS384':
 		case 'HS512': {
-			const bits = algorithm.slice(2)
-			const hashAlg = `sha${bits}`
+			const params = hmacAlgParams[algorithm]
+
+			if (key.type !== params.type)
+				throw new Error(
+					`Invalid key type for ${algorithm}. Expected "${params.type}", got "${key.type}".`
+				)
+
+			if (
+				key.symmetricKeySize === undefined ||
+				key.symmetricKeySize < params.minKeyBytes
+			)
+				throw new Error(
+					`Key is too short for ${algorithm}. Expected at least ${params.minKeyBytes} bytes, got ${key.symmetricKeySize} bytes.`
+				)
 
 			// For HMAC, we compute the signature again and compare
-			const expectedSignature = createHmac(hashAlg, key)
+			const expectedSignature = createHmac(params.hashAlg, key)
 				.update(signingInput)
 				.digest()
 
@@ -33,8 +53,28 @@ export const verifySignature = (
 		case 'RS256':
 		case 'RS384':
 		case 'RS512': {
-			const hashAlg = `sha${algorithm.slice(2)}`
-			return createVerify(hashAlg)
+			const params = rsaAlgParams[algorithm]
+
+			if (key.type !== params.verifyKeyType)
+				throw new Error(
+					`Invalid key type for ${algorithm} during verification. Expected "${params.verifyKeyType}", got "${key.type}".`
+				)
+
+			if (key.asymmetricKeyType !== params.asymmetricKeyType)
+				throw new Error(
+					`Invalid key type for ${algorithm}. Expected "${params.asymmetricKeyType}", got "${key.asymmetricKeyType}".`
+				)
+
+			// Check if the key size is large enough
+			const keySizeInBits = key.asymmetricKeyDetails?.modulusLength
+
+			if (!keySizeInBits || keySizeInBits < params.minKeyBits) {
+				throw new Error(
+					`Key size for ${algorithm} is too small. Expected at least ${params.minKeyBits} bits, got ${keySizeInBits} bits.`
+				)
+			}
+
+			return createVerify(params.hashAlg)
 				.update(signingInput)
 				.verify(key, signature)
 		}
@@ -43,8 +83,25 @@ export const verifySignature = (
 		case 'ES256':
 		case 'ES384':
 		case 'ES512': {
-			const hashAlg = `sha${algorithm.slice(2)}`
-			return createVerify(hashAlg)
+			const params = ecdsaAlgParams[algorithm]
+
+			if (key.type !== params.verifyKeyType)
+				throw new Error(
+					`Invalid key type for ${algorithm} during verification. Expected "${params.verifyKeyType}", got "${key.type}".`
+				)
+
+			if (key.asymmetricKeyType !== params.asymmetricKeyType)
+				throw new Error(
+					`Invalid key type for ${algorithm}. Expected "${params.asymmetricKeyType}", got "${key.asymmetricKeyType}".`
+				)
+
+			if (key.asymmetricKeyDetails?.namedCurve !== params.namedCurve)
+				throw new Error(`Invalid curve for ${algorithm}.`)
+
+			if (signature.length !== params.signatureBytes)
+				throw new Error(`Signature is not 64 bytes`)
+
+			return createVerify(params.hashAlg)
 				.update(signingInput)
 				.verify(key, signature)
 		}
@@ -53,8 +110,26 @@ export const verifySignature = (
 		case 'PS256':
 		case 'PS384':
 		case 'PS512': {
-			const hashAlg = `sha${algorithm.slice(2)}`
-			return createVerify(hashAlg)
+			const params = rsaPssAlgParams[algorithm]
+			if (key.type !== params.verifyKeyType)
+				throw new Error(
+					`Invalid key type for ${algorithm} during verification. Expected "${params.verifyKeyType}", got "${key.type}".`
+				)
+
+			if (key.asymmetricKeyType !== params.asymmetricKeyType) {
+				throw new Error(
+					`Invalid key type for ${algorithm}. Expected "${params.asymmetricKeyType}", got "${key.asymmetricKeyType}".`
+				)
+			}
+
+			const keySizeInBits = key.asymmetricKeyDetails?.modulusLength
+			if (!keySizeInBits || keySizeInBits < params.minKeyBits) {
+				throw new Error(
+					`Key size for ${algorithm} is too small. Expected at least ${params.minKeyBits} bits, got ${keySizeInBits} bits.`
+				)
+			}
+
+			return createVerify(params.hashAlg)
 				.update(signingInput)
 				.verify(
 					{
