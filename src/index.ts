e056171a913d87b73144ba811b3d7b854350526e
Ömer Toraman <toramanomer@icloud.com>
2025-02-27 23:33:24 +0300

    Add exports for JWS creation and verification functions

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { createCompactJws } from '@/serialization/compact/createJws'
+export { verifyCompactJws } from '@/serialization/compact/verifyJws'
+
+export { createFlattenedJws } from '@/serialization/flattened/createJws'
+export { verifyFlattenedJws } from '@/serialization/flattened/verifyJws'
+
+export { createGeneralJws } from '@/serialization/general/createJws'
