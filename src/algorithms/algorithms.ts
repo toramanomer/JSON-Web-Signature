254cecb4cb6930b4f95349a574d944af4f4829d0
Ömer Toraman <toramanomer@icloud.com>
2025-03-01 00:26:16 +0300

    Refactor algorithm exports to use parameterized imports for HMAC, RSA, ECDSA, and RSASSA-PSS

--- src/algorithms/algorithms.ts
+++ src/algorithms/algorithms.ts
@@ -1,23 +1,10 @@
-export const Algorithms = {
-	// HMAC with SHA-2 Functions
-	HS256: 'HS256',
-	HS384: 'HS384',
-	HS512: 'HS512',
+import { hmacParams } from './hmac/params'
+import { rsaParams } from './rsa/params'
+import { ecdsaParams } from './ecdsa/params'
+import { rsaPssParams } from './rsa-pss/params'
 
-	// Digital Signature with RSASSA-PKCS1-v1_5
-	RS256: 'RS256',
-	RS384: 'RS384',
-	RS512: 'RS512',
-
-	// Digital Signature with ECDSA
-	ES256: 'ES256',
-	ES384: 'ES384',
-	ES512: 'ES512',
-
-	// Digital Signature with RSASSA-PSS
-	PS256: 'PS256',
-	PS384: 'PS384',
-	PS512: 'PS512'
-}
-
-export type Algorithm = keyof typeof Algorithms
+export type Algorithm =
+	| keyof typeof hmacParams
+	| keyof typeof rsaParams
+	| keyof typeof ecdsaParams
+	| keyof typeof rsaPssParams
