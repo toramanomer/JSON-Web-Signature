2774aba34a338d2d14cb319cf3ed3d731cd17dd0
Ömer Toraman <toramanomer@icloud.com>
2025-03-04 23:49:13 +0300

    Add unit tests for signRsa function to validate key usage scenarios

---
+++ test/unit/algorithms/rsa/sign.test.ts
@@ -0,0 +1,71 @@
+import { generateKeyPairSync, generateKeySync } from 'node:crypto'
+import { describe, it, expect } from 'vitest'
+
+import { signRsa } from '../../../../src/algorithms/rsa/sign'
+import { InvalidKeyError } from '../../../../src/algorithms/InvalidKeyError'
+import {
+	rsaParams,
+	type RsaAlgorithm
+} from '../../../../src/algorithms/rsa/params'
+
+const signingInput = 'test'
+
+describe('signRsa', () => {
+	describe('When using invalid key', () => {
+		const algorithms = Object.keys(rsaParams) as RsaAlgorithm[]
+
+		// Secret key
+		it.for(algorithms)(
+			'should throw when secret key is used with %s',
+			algorithm => {
+				const key = generateKeySync('hmac', { length: 256 })
+
+				expect(() => signRsa({ algorithm, key, signingInput })).toThrow(
+					InvalidKeyError
+				)
+			}
+		)
+
+		// Public key
+		it.for(algorithms)(
+			'should throw when public key is used with %s',
+			algorithm => {
+				const { publicKey: key } = generateKeyPairSync('rsa', {
+					modulusLength: rsaParams[algorithm].minKeyBits
+				})
+
+				expect(() => signRsa({ algorithm, key, signingInput })).toThrow(
+					InvalidKeyError
+				)
+			}
+		)
+
+		// Wrong asymmetric key type
+		it.for(algorithms)(
+			'should throw when EC key is used with %s',
+			algorithm => {
+				const { privateKey: key } = generateKeyPairSync('ec', {
+					namedCurve: 'P-256'
+				})
+
+				expect(() => signRsa({ algorithm, key, signingInput })).toThrow(
+					InvalidKeyError
+				)
+			}
+		)
+
+		// Key too small
+		it.for(algorithms)(
+			'should throw when key size is too small for %s',
+			algorithm => {
+				const { privateKey: key } = generateKeyPairSync('rsa', {
+					modulusLength: rsaParams[algorithm].minKeyBits - 1
+				})
+
+				expect(() => signRsa({ algorithm, key, signingInput })).toThrow(
+					InvalidKeyError
+				)
+			}
+		)
+	})
+})
