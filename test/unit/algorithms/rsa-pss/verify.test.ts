ac4941fd4c588c9a2fa80bc752fded7bbcf35fb2
Ömer Toraman <toramanomer@icloud.com>
2025-03-05 00:11:18 +0300

    Add unit tests for verifyRsaPss function to validate key usage scenarios

---
+++ test/unit/algorithms/rsa-pss/verify.test.ts
@@ -0,0 +1,71 @@
+import { generateKeyPairSync, generateKeySync } from 'node:crypto'
+import { describe, it, expect } from 'vitest'
+
+import { verifyRsaPss } from '../../../../src/algorithms/rsa-pss/verify'
+import { InvalidKeyError } from '../../../../src/algorithms/InvalidKeyError'
+import {
+	rsaPssParams,
+	type RsaPssAlgorithm
+} from '../../../../src/algorithms/rsa-pss/params'
+
+describe('verifyRsaPss', () => {
+	describe('When using invalid key', () => {
+		const algorithms = Object.keys(rsaPssParams) as RsaPssAlgorithm[]
+		const signature = Buffer.from('dummy-signature')
+		const signingInput = 'test'
+
+		// Secret key
+		it.for(algorithms)(
+			'should throw when secret key is used with %s',
+			algorithm => {
+				const key = generateKeySync('hmac', { length: 256 })
+
+				expect(() =>
+					verifyRsaPss({ algorithm, key, signature, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Private key
+		it.for(algorithms)(
+			'should throw when private key is used with %s',
+			algorithm => {
+				const { privateKey: key } = generateKeyPairSync('rsa-pss', {
+					modulusLength: rsaPssParams[algorithm].minKeyBits
+				})
+
+				expect(() =>
+					verifyRsaPss({ algorithm, key, signature, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Wrong asymmetric key type
+		it.for(algorithms)(
+			'should throw when EC key is used with %s',
+			algorithm => {
+				const { publicKey: key } = generateKeyPairSync('ec', {
+					namedCurve: 'P-256'
+				})
+
+				expect(() =>
+					verifyRsaPss({ algorithm, key, signature, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Key too small
+		it.for(algorithms)(
+			'should throw when key size is too small for %s',
+			algorithm => {
+				const { publicKey: key } = generateKeyPairSync('rsa-pss', {
+					modulusLength: rsaPssParams[algorithm].minKeyBits - 1
+				})
+
+				expect(() =>
+					verifyRsaPss({ algorithm, key, signature, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+	})
+})
