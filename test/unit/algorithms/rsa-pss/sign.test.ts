659bbee433c5f6df65e29ed2bfab74cdee505c05
Ömer Toraman <toramanomer@icloud.com>
2025-03-04 23:55:37 +0300

    Add unit tests for signRsaPss function to validate key usage scenarios

---
+++ test/unit/algorithms/rsa-pss/sign.test.ts
@@ -0,0 +1,71 @@
+import { generateKeyPairSync, generateKeySync } from 'node:crypto'
+import { describe, it, expect } from 'vitest'
+
+import { signRsaPss } from '../../../../src/algorithms/rsa-pss/sign'
+import { InvalidKeyError } from '../../../../src/algorithms/InvalidKeyError'
+import {
+	rsaPssParams,
+	type RsaPssAlgorithm
+} from '../../../../src/algorithms/rsa-pss/params'
+
+const signingInput = 'test'
+
+describe('signRsaPss', () => {
+	describe('When using invalid key', () => {
+		const algorithms = Object.keys(rsaPssParams) as RsaPssAlgorithm[]
+
+		// Secret key
+		it.for(algorithms)(
+			'should throw when secret key is used with %s',
+			algorithm => {
+				const key = generateKeySync('hmac', { length: 256 })
+
+				expect(() =>
+					signRsaPss({ algorithm, key, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Public key
+		it.for(algorithms)(
+			'should throw when public key is used with %s',
+			algorithm => {
+				const { publicKey: key } = generateKeyPairSync('rsa-pss', {
+					modulusLength: rsaPssParams[algorithm].minKeyBits
+				})
+
+				expect(() =>
+					signRsaPss({ algorithm, key, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Wrong asymmetric key type
+		it.for(algorithms)(
+			'should throw when EC key is used with %s',
+			algorithm => {
+				const { privateKey: key } = generateKeyPairSync('ec', {
+					namedCurve: 'P-256'
+				})
+
+				expect(() =>
+					signRsaPss({ algorithm, key, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+
+		// Key too small
+		it.for(algorithms)(
+			'should throw when key size is too small for %s',
+			algorithm => {
+				const { privateKey: key } = generateKeyPairSync('rsa-pss', {
+					modulusLength: rsaPssParams[algorithm].minKeyBits - 1
+				})
+
+				expect(() =>
+					signRsaPss({ algorithm, key, signingInput })
+				).toThrow(InvalidKeyError)
+			}
+		)
+	})
+})
